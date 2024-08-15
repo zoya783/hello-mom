72d25ab2f476f319de4ca6a331afa632282616bc
zoya783 <zoyakor14@gmail.com>
2024-08-15 02:11:30 -0400

    init

---
+++ index.js
@@ -0,0 +1,4 @@
+
+const button = document.querySelector('button');
+const output = document.querySelector('output');
+button.addEventListener('click', function() {output.innerHTML = 'Mother I love you';});
